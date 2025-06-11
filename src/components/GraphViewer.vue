099a0d95782dad3e3708d566f0155c4e36fb18ee
sophXmoore1 <116595477+sophXmoore1@users.noreply.github.com>
2025-06-11 13:34:52 -0400

    Add comments

--- src/components/GraphViewer.vue
+++ src/components/GraphViewer.vue
@@ -10,6 +10,8 @@ import {useStore} from '@/stores/store'
 
 onMounted(async () => {
   const store = useStore()
+  
+  //Step 1: Get the nodes and edges
   const [nodesRes, edgesRes] = await Promise.all([
       fetch('/nodes.json'),
       fetch('/edges.json')
@@ -17,12 +19,15 @@ onMounted(async () => {
 
   const nodeData = await nodesRes.json();
   const edgeData = await edgesRes.json();
-  
+
+  //Step 2: Format the data into the structure expected by ForceGraph3D and store it globally
   let formattedGraphJson = convertToGraphFormat(nodeData, edgeData)
   store.graphJSON = formattedGraphJson
 
+  //Step 3: Get the HTML element where we want to render the graph
   let graphDiv = document.getElementById('3d-graph')
 
+  //Step 4: Create the ForceGraph3D (https://github.com/vasturiano/3d-force-graph)
   const Graph = new ForceGraph3D(graphDiv)
     .graphData(formattedGraphJson)
     .nodeLabel('nodeLabel')
